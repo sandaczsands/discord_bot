feef78eeeda3a847e37cdcb3910d2255e1d551f3
sandaczsands <aleksandra.ostrowski.pl@gmail.com>
2025-06-17 14:20:10 +0200

    chronological order in summarizer and qa functions

--- qa.py
+++ qa.py
@@ -3,7 +3,7 @@ def answer_question(question, context, model, tokenizer):
         return "Zbyt mało kontekstu, by odpowiedzieć."
 
     input_text = f"question: {question} context: {context}"
-    print(f"Input for question answering: {input_text[:100]}...")  # Debugging line to check input
+    print(f"Input for question answering: {input_text[:100]}") 
     input_ids = tokenizer(input_text, return_tensors="pt", truncation=True, max_length=512).input_ids
     output_ids = model.generate(input_ids, max_length=100)
     answer = tokenizer.decode(output_ids[0], skip_special_tokens=True)
